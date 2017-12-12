6b1f158569e0586dc83fb0bc4eaa5dfb648ab8a0
Vova Kulikov <vovakulikov@icloud.com>
2017-12-12 23:07:39 +0300

    added basis first section

---
+++ src/components/+header/header.js
@@ -0,0 +1,8 @@
+
+// self file styles
+import './header.scss';
+
+// child components styles
+import './components/logo/logotype.scss';
+import './components/menu/menu.scss'
+import './components/button/custom-button.scss'
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+
+// global styles file
 import './index.scss';
+
+// components styles file
+import './components/+header/header';
